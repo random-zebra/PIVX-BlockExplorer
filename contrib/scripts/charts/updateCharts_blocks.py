0cd96a8abeb3bb8c11b19d8de8103cfcab6a2c63
random-zebra <random.zebra@protonmail.com>
2019-06-24 15:51:17 +0200

    Charts: use getblockindexstats RPC in python cronjob

--- contrib/scripts/charts/updateCharts_blocks.py
+++ contrib/scripts/charts/updateCharts_blocks.py
@@ -21,9 +21,6 @@ rpc_port = "18049" if isTestnet else "8049"
 rpc_url = "http://%s:%s@%s" % (rpc_user, rpc_pass, rpc_host)
 bb_url = "https://testnet" if isTestnet else "https://explorer"
 bb_url += ".pivx.link"
-# CHAIN PARAMS
-nFirstZPivBlock = 201576 if isTestnet else 863787
-nLastPOWBlock = 200 if isTestnet else 259200
 
 # FILENAME
 supply_data_file = os.path.join("/opt/coins/blockbook/plot_data", "supply_data.json")
@@ -50,44 +47,7 @@ class ApiClient:
 
 
 # --------------------------------------------------
-# Helper functions
-
-def GetFeesInBlock(block):
-    fees = 0
-    tx_count = len(block["tx"])
-    if (
-            (block["height"] <= nLastPOWBlock and tx_count > 1) or
-            (block["height"] > nLastPOWBlock and tx_count > 2)
-    ):  # skip coinbase/coinstake
-        for i in range(1, tx_count):
-            tx_id = block["tx"][i]
-            fees += int(bb_conn.getTx(tx_id).get('fees'))
-    return fees/1e8
-
-# --------------------------------------------------
 # Constants
-
-key_to_int = {
-    "denom_1": 1,
-    "denom_5": 5,
-    "denom_10": 10,
-    "denom_50": 50,
-    "denom_100": 100,
-    "denom_500": 500,
-    "denom_1000": 1000,
-    "denom_5000": 5000
-}
-
-key_to_key = {
-    "denom_1": "Number of 1-denom mints",
-    "denom_5": "Number of 5-denom mints",
-    "denom_10": "Number of 10-denom mints",
-    "denom_50": "Number of 50-denom mints",
-    "denom_100": "Number of 100-denom mints",
-    "denom_500": "Number of 500-denom mints",
-    "denom_1000": "Number of 1000-denom mints",
-    "denom_5000": "Number of 5000-denom mints"
-}
 
 ZC_DENOMS = [1, 5, 10, 50, 100, 500, 1000, 5000]
 
@@ -96,7 +56,6 @@ ZC_DENOMS = [1, 5, 10, 50, 100, 500, 1000, 5000]
 # Initialize RPC connection and API client
 httpConnection = httplib.HTTPConnection(rpc_host, rpc_port, timeout=20)
 conn = AuthServiceProxy(rpc_url, timeout=1000, connection=httpConnection)
-bb_conn = ApiClient()
 
 # Read data from file
 try:
@@ -107,47 +66,59 @@ try:
         network_data = json.load(f)
 
 except FileNotFoundError:
-    # first run
+    # first run (blocks 0-99 and 100-199 hardcoded)
     supply_data = {}
-    supply_data["blocks_axis"] = []
-    supply_data["time_axis"] = []
-    supply_data["lastBlockNum"] = 0
-    supply_data["lastBlockHash"] = ''
-    supply_data["pivSupply"] = []
+    supply_data["blocks_axis"] = [0, 100]
+    if isTestnet:
+        supply_data["time_axis"] = [1454124731, 1488951557]
+        supply_data["lastBlockHash"] = '0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818'
+        supply_data["pivSupply"] = [0.0, 24810000.99975790]
+    else:
+        supply_data["time_axis"] = [1454124731, 1454186818]
+        supply_data["lastBlockHash"] = '0000000a86f23294329c83d69e254a4f8d127b6b899a14b147885740c4be1713'
+        supply_data["pivSupply"] = [0.0, 84751.0]
     zpivSupply = {}
     zpivMints = {}
     for k in ZC_DENOMS:
         denom_key = "denom_%d" % k
-        zpivSupply[denom_key] = []
-        zpivMints[denom_key] = []
+        zpivSupply[denom_key] = [0, 0]
+        zpivMints[denom_key] = [0, 0]
     supply_data["zpivSupply"] = zpivSupply
     supply_data["zpivMints"] = zpivMints
 
     network_data = {}
-    network_data["blocks_axis"] = []
-    network_data["time_axis"] = []
-    network_data["txs"] = []
-    network_data["size"] = []
-    network_data["blocktime"] = []
-    network_data["difficulty"] = []
-    network_data["fees"] = []
-
+    network_data["blocks_axis"] = [0, 100]
+    if isTestnet:
+        network_data["time_axis"] = [1454124731, 1488951557]
+        network_data["txs"] = [0, 107]
+        network_data["fees_ttl"] = [0, 0.0003067]
+        network_data["fees_perKb"] = [0, 0.00012864]
+        network_data["blocksize"] = [0, 214]
+        network_data["blocktime"] = [0, (1488951557-1454124731)/100]
+        network_data["difficulty"] = [0.00, 0.00]
+    else:
+        network_data["time_axis"] = [1454124731, 1454186818]
+        network_data["txs"] = [0, 100]
+        network_data["fees_ttl"] = [0, 0.0027982]
+        network_data["fees_perKb"] = [0, 0.00011946]
+        network_data["blocksize"] = [0, 196]
+        network_data["blocktime"] = [0, (1454186818-1454124731)/100]
+        network_data["difficulty"] = [0.00, 0.09]
 
 
 # Check if a reorg occurred
+last_block_hash = conn.getblockhash(supply_data["blocks_axis"][-1])
 if (
-        len(supply_data["blocks_axis"]) > 3 and
-        conn.getblockhash(supply_data["lastBlockNum"]) != supply_data["lastBlockHash"]
+        len(supply_data["blocks_axis"]) > 6 and
+        last_block_hash != supply_data["lastBlockHash"]
 ):
     # remove 3 datapoints to be extra safe
-    for data_key in supply_data:
-        if data_key in ["zpivSupply", "zpivMints"]:
-            for denom_key in supply_data[data_key]:
-                supply_data[data_key][denom_key] = supply_data[data_key][denom_key][:-3]
-        elif data_key != "lastBlockNum":
-            supply_data[data_key] = supply_data[data_key][:-3]
-        else:
-            supply_data[data_key] -= 300
+    supply_data["lastBlockHash"] = last_block_hash
+    for data_key in ["zpivSupply", "zpivMints"]:
+        for denom_key in supply_data[data_key]:
+            supply_data[data_key][denom_key] = supply_data[data_key][denom_key][:-3]
+    for data_key in ["blocks_axis", "time_axis", "pivSupply"]:
+        supply_data[data_key] = supply_data[data_key][:-3]
 
     for data_key in network_data:
         network_data[data_key] = network_data[data_key][:-3]
@@ -156,65 +127,41 @@ if (
 # Add new data points
 blockCount = conn.getblockcount()
 
-while supply_data["lastBlockNum"] + 100 <= blockCount:
-    tx_count = 0
-    fees = 0
-    block_time = []
-    for i in range(1, 99):
-        # get intermediate block
-        hash = conn.getblockhash(supply_data["lastBlockNum"] + i)
-        inner_block = conn.getblock(hash, True)
-        # get tx count and blocktime (resetting last)
-        tx_count += len(inner_block["tx"])
-        if i == 1:
-            block_time.append(0)
-        else:
-            block_time.append(int(inner_block["time"]) - last_time)
-        last_time = int(inner_block["time"])
-        # get fees - skip for now
-        #fees += GetFeesInBlock(inner_block)
-
-
-    supply_data["lastBlockNum"] += 100
-    supply_data["lastBlockHash"] = conn.getblockhash(supply_data["lastBlockNum"])
-    print("Getting block %d..." % supply_data["lastBlockNum"])
+while supply_data["blocks_axis"][-1] + 100 <= blockCount:
+    # fetch block N+100
+    new_block_num = supply_data["blocks_axis"][-1] + 100
+    supply_data["lastBlockHash"] = conn.getblockhash(new_block_num)
+    print("Getting block %d..." % new_block_num)
     block = conn.getblock(supply_data["lastBlockHash"], True)
-    tx_count += len(block["tx"])
-    block_time.append(int(block["time"])-last_time)
-    #fees += GetFeesInBlock(block)
 
-    # Compute inner_block totals/avgs and reset counters
-    network_data["txs"].append(tx_count)
-    tx_count = 0
-    network_data["blocktime"].append(int(round(sum(block_time[1:]) / len(block_time[1:]))))
-    block_time = []
-    network_data["fees"] = round(fees, 8)
-    fees = 0
-
-
-    # get PIV supply, time,  diff, size
+    # get PIV supply and zPIV supply
     supply_data["pivSupply"].append(float(block["moneysupply"]))
+    for k in ZC_DENOMS:
+        supply_data["zpivSupply"]["denom_%d" % k].append(int(block["zPIVsupply"][str(k)]))
+
+    # get time, blocktime, blocksize and difficulty
     supply_data["time_axis"].append(int(block["time"]))
     network_data["time_axis"].append(int(block["time"]))
     network_data["difficulty"].append(float(round(block["difficulty"], 2)))
-    network_data["size"].append(int(block["size"]))
+    network_data["blocktime"].append((network_data["time_axis"][-1]-network_data["time_axis"][-2])/100)
+    network_data["blocksize"].append(int(block["size"]))
 
-    # get zPIV supply
+    # fetch blockindexstats over 100 blocks: [N, N+99]
+    block_stats = conn.getblockindexstats(new_block_num-100, 100)
+
+    # get mints
     for k in ZC_DENOMS:
-        if supply_data["lastBlockNum"] < nFirstZPivBlock:
-            supply_data["zpivSupply"]["denom_%d" % k].append(0)
-            supply_data["zpivMints"]["denom_%d" % k].append(0)
-            continue
-
-        supply_data["zpivSupply"]["denom_%d" % k].append(int(block["zPIVsupply"][str(k)]))
         # get mints in range
-        supply_data["zpivMints"]["denom_%d" % k].append(
-            int(conn.getmintsinblocks(supply_data["lastBlockNum"] - 99, 100, k)["Number of %d-denom mints" % k])
-        )
+        supply_data["zpivMints"]["denom_%d" % k].append(int(block_stats["mintcount"]["denom_%d" % k]))
+
+    # get tx count and fees
+    network_data["txs"].append(int(block_stats["txcount_all"]))
+    network_data["fees_ttl"].append(float(block_stats["ttlfee_all"]))
+    network_data["fees_perKb"].append(float(block_stats["feeperkb"]))
 
     # update range
-    supply_data["blocks_axis"].append(supply_data["lastBlockNum"])
-    network_data["blocks_axis"].append(supply_data["lastBlockNum"])
+    supply_data["blocks_axis"].append(new_block_num)
+    network_data["blocks_axis"].append(new_block_num)
 
 try:
     # Save to files
@@ -225,7 +172,7 @@ try:
         json.dump(network_data, f)
 
     # close connection
-    conn.close()
+    httpConnection.close()
 
 except Exception as e:
     print(e)
--- static/js/charts_network.js
+++ static/js/charts_network.js
@@ -1,7 +1,10 @@
 // blocks data
 const block_data = JSON.parse(network_data_Json);
+var bdata_totals = {
+    txs: [],
+    fees: []
+};
 const LAST_BLOCK_NUM = block_data.blocks_axis[block_data.blocks_axis.length-1];
-let fees_byte = [];
 
 // charts DOM elements
 const difficulty_ctx = document.getElementById("canv_net_01_ctx");
@@ -74,18 +77,19 @@ function GetBlocksizeDataPoints(bl_from, bl_to) {
     const step = 1+Math.floor((bl_to-bl_from) / 120);
 
     let tx_count = 0;
-    let tot_tx_count = 0;
+    let size = 0;
     for (let i = bl_from; i <= bl_to; i++) {
         tx_count += block_data.txs[i];
-        tot_tx_count += block_data.txs[i];
+        size += block_data.blocksize[i];
         if (step <= 1 || i % step == 0) {
             // add data to rangeObj
             rangeObj.blocks_axis.push(block_data.blocks_axis[i]);
             rangeObj.time_axis.push(new Date(block_data.time_axis[i]*1000).toLocaleString());
-            rangeObj.blocksize.push(block_data.size[i]);
+            rangeObj.blocksize.push(size/step);
             rangeObj.txes.push(tx_count/(100*step));
-            rangeObj.tot_txes.push(tot_tx_count);
+            rangeObj.tot_txes.push(bdata_totals.txs[i]);
             tx_count = 0;
+            size = 0;
         }
     }
 
@@ -98,7 +102,6 @@ function GetFeeDataPoints(bl_from, bl_to) {
     var rangeObj = {
         blocks_axis: [],
         time_axis: [],
-        block_fees: [],
         tot_fees: [],
         avg_fees: [],
     };
@@ -106,16 +109,13 @@ function GetFeeDataPoints(bl_from, bl_to) {
     // Limit to ~120 points
     const step = 1+Math.floor((bl_to-bl_from) / 120);
 
-    let tot_fees = 0;
     for (let i = bl_from; i <= bl_to; i++) {
-        tot_fees += block_data.fees[i];
         if (step <= 1 || i % step == 0) {
             // add data to rangeObj
             rangeObj.blocks_axis.push(block_data.blocks_axis[i]);
             rangeObj.time_axis.push(new Date(block_data.time_axis[i]*1000).toLocaleString());
-            rangeObj.block_fees.push(block_data.fees[i]);
-            rangeObj.tot_fees.push(AmountToDecimal(tot_fees, 8));
-            rangeObj.avg_fees.push(fees_byte[i]);
+            rangeObj.tot_fees.push(bdata_totals.fees[i]);
+            rangeObj.avg_fees.push(block_data.fees_perKb[i]);
         }
     }
     return rangeObj;
@@ -157,8 +157,7 @@ function SetFeeChartRange(val_from, val_to) {
      // update chart
     SetDataLabel(feeChart, rangeObj);
     feeChart.data.datasets[0].data = rangeObj.avg_fees;
-    feeChart.data.datasets[1].data = rangeObj.block_fees;
-    feeChart.data.datasets[2].data = rangeObj.tot_fees;
+    feeChart.data.datasets[1].data = rangeObj.tot_fees;
     // draw chart
     const canvID = fee_ctx.id.substring(0, fee_ctx.id.length-4);
     ShowCanvas(canvID);
@@ -167,19 +166,24 @@ function SetFeeChartRange(val_from, val_to) {
 
 // WINDOWS ON LOAD
 window.onload = function () {
-    ComputeAverages();
+    ComputeTotals();
     InitAllCharts();
 }
 
 // --- INIT FUNCTIONS ---
 
-function ComputeAverages() {
-    for (let i = 0; i < block_data.blocks_axis.length; i++) {
-        if (block_data.txs > 1) {
-            fees_byte.push(AmountToDecimal(block_data.fees / (block_data.size - 180), 8));
-        } else {
-            fees_byte.push(0);
-        }
+// Sums
+function ComputeTotals() {
+    let txs = 0;
+    for (let i = 0; i < block_data.txs.length; i++) {
+        txs += block_data.txs[i];
+        bdata_totals.txs.push(txs);
+    }
+
+    let fees = 0;
+    for (let i = 0; i < block_data.fees_ttl.length; i++) {
+        fees += AmountToDecimal(block_data.fees_ttl[i], 8);
+        bdata_totals.fees.push(fees);
     }
 }
 
@@ -265,9 +269,8 @@ function InitBlockSizeChart() {
 
 function InitFeeChart() {
     let legend1 = 'Avg. Fee per byte (PIV)';
-    let legend2 = 'Fees in blocks (PIV)';
-    let legend3 = 'TOT Burnt Fees (PIV)'
-    feeChart = InitBarChart(fee_ctx, [legend1, legend2, legend3]);
+    let legend2 = 'TOT Burnt Fees (PIV)'
+    feeChart = InitLineChart(fee_ctx, [legend1, legend2]);
     feeChart.options.tooltips.callbacks.title = tooltipTitle;
     feeChart.data.labelset = "blocks"
     feeChart.data.datasets = [
@@ -283,21 +286,13 @@ function InitFeeChart() {
         },
         {
             data: [],
-            label: "fees in blocks",
-            yAxisID: 'axis1',
-            borderColor: 'rgba(244, 217, 65, 0.7)',
-            backgroundColor: 'rgba(244, 217, 65, 0.3)',
-            borderWidth: 1
-        },
-        {
-            data: [],
             label: "total fees",
             borderColor: 'rgba(167, 66, 244, 0.8)',
             pointRadius: 2,
             needsRadius: 2,   // for resize,
             fill: false,
             type: 'line',
-            yAxisID: 'axis2'
+            yAxisID: 'axis1'
         },
     ];
     feeChart.options.onResize = MinimizeChartLegend;
