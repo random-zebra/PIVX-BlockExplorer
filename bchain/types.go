fb644922e80232e91deceba21324c4311056e646
random-zebra <random.zebra@protonmail.com>
2021-01-05 19:10:43 +0100

    [Trivial] Fix vShieldSpend and vShieldOutput naming (drop 'ed' suffix)

--- bchain/types.go
+++ bchain/types.go
@@ -95,8 +95,8 @@ type Tx struct {
     Time             int64       `json:"time,omitempty"`
     Blocktime        int64       `json:"blocktime,omitempty"`
     // PIVX Shield
-    VShieldIn              []VShieldIn  `json:"vShieldedSpend,omitempty"`
-    VShieldOut             []VShieldOut `json:"vShieldedOutput,omitempty"`
+    VShieldIn              []VShieldIn  `json:"vShieldSpend,omitempty"`
+    VShieldOut             []VShieldOut `json:"vShieldOutput,omitempty"`
     ShieldValBal           big.Int          `json:"valueBalanceSat"`
 
     CoinSpecificData interface{} `json:"-"`
